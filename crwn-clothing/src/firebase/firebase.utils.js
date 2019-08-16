4a7fcc82de0243e7e0f1aebfcb8291d37d0c8329
NhanNguyen001 <nhan2151@gmail.com>
2019-08-17 01:29:53 +0700

    added redux, and implemented userReducer and userActions to header and app components

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -21,6 +21,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   const snapShot = await userRef.get();
   if(!snapShot.exists) {
+    console.log(userAuth);
     const { displayName, email } = userAuth;
     const createdAt = new Date();
     try {
