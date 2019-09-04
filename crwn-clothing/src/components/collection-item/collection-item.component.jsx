6a273d0e94db0a737cd770714f5020cafe500325
NhanNguyen001 <nhan2151@gmail.com>
2019-09-05 00:13:47 +0700

    Data Normalization + Collection Page --- Done

--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -9,27 +9,29 @@ import './collection-item.styles.scss';
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
   return (
-    <div className='collection-item'>
+    <div className="collection-item">
       <div
-        className='image'
+        className="image"
         style={{
           backgroundImage: `url(${imageUrl})`
-        }} />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
-      <CustomButton 
-        onClick={() => addItem(item)} 
-        inverted
-      > 
-        Add to cart 
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
       </CustomButton>
     </div>
-)};
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
-})
+});
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
