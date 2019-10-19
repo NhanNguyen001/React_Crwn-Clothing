1856930bcb10aee6bbe047cc4aa4fd8b7725d85a
Nhan Nguyen Thanh <giup2151@gmail.com>
2019-10-20 00:34:04 +0700

    Adding withSpinner to Project

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ import App from './App';
 import { store, persistor } from './redux/store';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
-		</BrowserRouter>
-	</Provider>,
-	document.getElementById('root')
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
