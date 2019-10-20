295f76f17fceba435cf20061275b46e8c537bb90
NhanNguyen001 <nhan2151@gmail.com>
2019-10-20 20:51:16 +0700

    Implement code that pulls in our shop data from firestore and store it into the shop reducer from our shop page. Also included our withSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -27,7 +27,13 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.onSnapshot(async snapshot => {
+    // fetch(
+    //   'https://firestore.googleapis.com/v1/projects/crwn-clothing-60481/databases/(default)/documents/collections'
+    // )
+    //   .then(response => response.json())
+    //   .then(collections => console.log(collections));
+
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
