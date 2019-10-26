a3dcd2dcc081184b4437aaddac4d74c66e97b5ae
NhanNguyen001 <nhan2151@gmail.com>
2019-10-26 19:50:49 +0700

    Converted Redux Thunk into Saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,17 +3,25 @@ import { createStore, applyMiddleware } from 'redux';
 // Declare persistStore
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+// const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 // Persistor is essentially a persisted version of store
 const persistor = persistStore(store);
